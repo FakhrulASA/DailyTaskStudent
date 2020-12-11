a7be1f7d82128f9475b3dcf7ce12aecfb1d34b7e
Antor <afakhrul9@gmail.com>
2020-12-11 23:49:18 +0600

    Ebook, Note, Event,Notice,direction

--- app/src/main/java/com/siddiqei/dailytaskbeta/adapter/ClassAdapter.java
+++ app/src/main/java/com/siddiqei/dailytaskbeta/adapter/ClassAdapter.java
@@ -1,16 +1,20 @@
 package com.siddiqei.dailytaskbeta.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.siddiqei.dailytaskbeta.R;
 import com.siddiqei.dailytaskbeta.model.ClassListModel;
+import com.siddiqei.dailytaskbeta.ui.CourseDetailsActivity;
 
 import java.util.List;
 
@@ -39,6 +43,17 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
           holder.subject.setText(posts.get(position).getName());
           holder.time.setText(posts.get(position).getTime());
           holder.day.setText(posts.get(position).getDays());
+          holder.cardView.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent=new Intent(context, CourseDetailsActivity.class);
+                  intent.putExtra("NAME",posts.get(position).getName());
+                  intent.putExtra("TIME",posts.get(position).getTime());
+                  intent.putExtra("DAY",posts.get(position).getDays());
+                  intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                  context.startActivity(intent);
+              }
+          });
     }
     //how many item
     @Override
@@ -48,12 +63,14 @@ public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ViewHolder> 
     //declaring the value
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView subject,teacher,time,day;
+        LinearLayout cardView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             //initialize
             subject=itemView.findViewById(R.id.subject_text);
             time=itemView.findViewById(R.id.time_text);
             day=itemView.findViewById(R.id.day_text);
+            cardView=itemView.findViewById(R.id.card_course);
         }
     }
 }
--- app/src/main/java/com/siddiqei/dailytaskbeta/ui/CourseDetailsActivity.java
+++ app/src/main/java/com/siddiqei/dailytaskbeta/ui/CourseDetailsActivity.java
@@ -2,15 +2,51 @@ package com.siddiqei.dailytaskbeta.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.siddiqei.dailytaskbeta.GPS.GpsTracker;
 import com.siddiqei.dailytaskbeta.R;
 
-public class CourseDetailsActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class CourseDetailsActivity extends AppCompatActivity {
+    TextView nameTV,timeTV,dayTV;
+    Button direction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+        nameTV=findViewById(R.id.classname_details);
+        timeTV=findViewById(R.id.time_details);
+        dayTV=findViewById(R.id.days_details);
+        direction=findViewById(R.id.direction);
+        String name=getIntent().getExtras().getString("NAME");
+        String time=getIntent().getExtras().getString("TIME");
+        String day=getIntent().getExtras().getString("DAY");
+        nameTV.setText(name);
+        timeTV.setText(time);
+        dayTV.setText(day);
+        GpsTracker gpsTracker=new GpsTracker(getApplicationContext());
+        double lat=gpsTracker.getLatitude();
+        double lon=gpsTracker.getLongitude();
+
+
+        direction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f(%s)&daddr=%f,%f (%s)", lat, lon, "Student Home", 23.7549, 90.3764, "Daffodil International University Classroom");
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
